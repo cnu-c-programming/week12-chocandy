a814c0a387bd4036d423e4ae2e64c2e256868346
chominjeong <chomj4836@gmail.com>
2026-05-28 22:30:59 +0900

    Update 3-1~5

--- code/3-5.c
+++ code/3-5.c
@@ -5,12 +5,19 @@ int main(int argc, const char* argv[]) {
 
     FILE* fp = fopen(filename, "r");
     if (fp == NULL) {
-        return 0;
+        return 1;
     }
 
-
     int sum = 0;
+    int num;
+    char buffer[256];
 
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        if (sscanf(buffer, "%d", &num) == 1)
+            sum += num;
+        else
+            fprintf(stderr, "invalid input: %s", buffer);
+    }
 
     printf("sum: %d\n", sum);
     fclose(fp);
